20113845548e09b73defe25b596a3e8263128883
Marco Fracassi <fracassi.marco@gmail.com>
2022-09-16 11:43:01 +0200

    Refactor

--- src/main/kotlin/io/fracassi/LandPerimeter.kt
+++ src/main/kotlin/io/fracassi/LandPerimeter.kt
@@ -1,17 +1,39 @@
 package io.fracassi
 
 fun landPerimeter(rows: Array<String>): String {
-    val totalPerimeter: Int = rows.first().mapIndexed { index, character ->
-        if(character != 'X')
-            0
-        else {
-            var perimeter = 4
-            if (index != 0 && rows.first()[index - 1] == 'X' ||
-                index != rows.first().length - 1 && rows.first()[index + 1] == 'X'
-            )
-                perimeter--
-            perimeter
-        }
-    }.sumOf { it }
-    return "Total land perimeter: ${totalPerimeter.toString()}"
+    val world = rows.first().map { asElement(it) }
+
+    val perimeter = world.mapIndexed { col, element ->
+        val neighbors = mutableListOf<Element>()
+        if(col != 0)
+            neighbors.add(world[col - 1])
+        if(col != world.size - 1)
+            neighbors.add(world[col + 1])
+        element.addNeighbors(neighbors)
+    }.sumOf { it.perimeter() }
+
+    return "Total land perimeter: $perimeter"
+}
+
+private fun asElement(it: Char) = when (it) {
+    'X' -> Land()
+    else -> Water()
+}
+
+interface Element {
+    fun addNeighbors(elements: List<Element>): Element
+    fun perimeter(): Int
+}
+
+class Land(private val neighbors: List<Element> = emptyList()) : Element {
+    override fun addNeighbors(elements: List<Element>) = Land(elements)
+
+    override fun perimeter(): Int {
+        return 4 - neighbors.count { it is Land }
+    }
+}
+
+class Water : Element {
+    override fun addNeighbors(elements: List<Element>)= this
+    override fun perimeter() = 0
 }
