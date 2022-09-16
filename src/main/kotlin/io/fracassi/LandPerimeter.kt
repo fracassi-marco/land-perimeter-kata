6449b9a92e1c2a7ad01317b9f4262f1dfa2a8ac2
Marco Fracassi <fracassi.marco@gmail.com>
2022-09-16 11:01:24 +0200

    add water

--- src/main/kotlin/io/fracassi/LandPerimeter.kt
+++ src/main/kotlin/io/fracassi/LandPerimeter.kt
@@ -1,16 +1,17 @@
 package io.fracassi
 
-class Land(vicini: Int) {
-
-}
-
 fun landPerimeter(rows: Array<String>): String {
-    val totalPerimeter = rows.first().mapIndexed { index, character ->
-        var perimeter = 4
-        if (index != 0 ||
-            index != rows.first().length - 1)
-            perimeter--
-        perimeter
-    }.sum()
-    return "Total land perimeter: $totalPerimeter"
+    val totalPerimeter: Int = rows.first().mapIndexed { index, character ->
+        if(character != 'X')
+            0
+        else {
+            var perimeter = 4
+            if (index != 0 && rows.first()[index - 1] == 'X' ||
+                index != rows.first().length - 1 && rows.first()[index + 1] == 'X'
+            )
+                perimeter--
+            perimeter
+        }
+    }.sumOf { it }
+    return "Total land perimeter: ${totalPerimeter.toString()}"
 }
--- src/test/kotlin/io/fracassi/LandPerimeterTest.kt
+++ src/test/kotlin/io/fracassi/LandPerimeterTest.kt
@@ -14,4 +14,19 @@ class LandPerimeterTest {
     fun `1x2 land in 1x2 world`() {
         assertThat(landPerimeter(arrayOf("XX"))).isEqualTo("Total land perimeter: 6")
     }
+
+    @Test
+    fun `1x2 land on the left of 1x3 world`() {
+        assertThat(landPerimeter(arrayOf("XXO"))).isEqualTo("Total land perimeter: 6")
+    }
+
+    @Test
+    fun `1x2 land on the right of 1x3 world`() {
+        assertThat(landPerimeter(arrayOf("OXX"))).isEqualTo("Total land perimeter: 6")
+    }
+
+    @Test
+    fun `1x1 and 1x1 lands in 1x4 world`() {
+        assertThat(landPerimeter(arrayOf("OXOX"))).isEqualTo("Total land perimeter: 8")
+    }
 }
